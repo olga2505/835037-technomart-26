29be2479e51ba0de2532fcf1ee0d2758e656e56e
Olga <151590@mail.ru>
2019-10-19 21:52:33 +0300

    доработки

--- js/color-add.js
+++ js/color-add.js
@@ -1,15 +1,37 @@
-  var catalogSave = document.querySelector(".catalog-save");
+  var cartPopup = document.querySelector(".popup-cart");
+  var cartClose = cartPopup.querySelector(".exit-cart");
   var bookmarks = document.querySelector(".bookmarks");
+  var bookmarksCount = bookmarks.querySelector("span");
 
-  var buy = document.querySelector(".catalog-buy");
   var basket = document.querySelector(".basket");
+  var basketCount = basket.querySelector("span")
 
-  catalogSave.addEventListener("click", function (evt) {
-    evt.preventDefault();
+document.addEventListener("click", function (evt) {
+  var target = evt.target;
+
+  if(target.classList.contains("catalog-save")) {
     bookmarks.classList.add("color-add");
-});
+    bookmarksCount.innerHTML = +bookmarksCount.innerHTML + 1;
+  }
 
-buy.addEventListener("click", function (evt) {
-    evt.preventDefault();
+  if(target.classList.contains("catalog-buy")) {
     basket.classList.add("color-add");
+    basketCount.innerHTML = +basketCount.innerHTML + 1;
+    cartPopup.classList.add("modal-show");
+
+    cartClose.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      cartPopup.classList.remove("modal-show");
+    });
+
+  }
+})
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (cartPopup.classList.contains("modal-show")) {
+      cartPopup.classList.remove("modal-show");
+    }
+  }
 });
